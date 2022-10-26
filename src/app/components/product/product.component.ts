cc5e6aa7bac52f593053a41aff022622f8b3a19c
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2022-10-26 14:44:16 +0300

    26 Ekim 2022

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 
@@ -13,9 +15,12 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded = false;
+  filterText="";
   
   constructor(private productService:ProductService,
               private activatedRoute:ActivatedRoute,
+              private toastrService:ToastrService,
+              private cartService:CartService
     ) { }
 
   ngOnInit(): void {
@@ -44,6 +49,11 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    this.toastrService.success("Sepete eklendi",product.productName);
   }
 
 }
